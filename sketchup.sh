2ac6fc112bed776fa8f747dc75e1cefd3ba6c16e
whoisterencelee <whoisterencelee@hotmail.com>
2024-07-20 07:59:16 +0800

    use online sketchup these days

--- sketchup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export WINEPREFIX="${HOME}/.sketchup64"
-# wine SketchUpMake-en-x64.exe
-# winetricks corefonts
-# cp $WINEPREFIX/drive_c/windows/syswow64/mfc100u.dll $WINEPREFIX/drive_c/windows/system32
-wine "C:\Program Files (x86)\SketchUp\SketchUp 2016\SketchUp.exe"
