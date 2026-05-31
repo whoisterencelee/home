b0a9d4a05ebf6244a78a23fbddac7a4dcaa1e958
whoisterencelee <terence.lee@sustaincia.org>
2026-05-31 09:09:26 +0800

    link vim to llm and also git diff

---
+++ .vim/plugin/llm.vim
@@ -0,0 +1,1 @@
+../vim_llm/llm.vim
---
+++ .vim/setup.sh
@@ -0,0 +1,2 @@
+git submodule add https://git.functional.limited/whoisterencelee/vim_llm.git
+git submodule add https://git.functional.limited/whoisterencelee/git-diff-staged.git
